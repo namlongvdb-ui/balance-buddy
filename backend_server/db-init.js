48ed333f755422f7a7587e6089e7b6fa9d546a69
namlongvdb-ui <namlongvdb@gmail.com>
2026-04-07 08:27:33 +0700

    Update db-init.js

--- backend_server/db-init.js
+++ backend_server/db-init.js
@@ -4,7 +4,7 @@ const { Pool } = require("pg");
 const pool = new Pool({
   host: process.env.DB_HOST || "10.24.16.77",
   port: parseInt(process.env.DB_PORT || "5432"),
-  database: process.env.DB_NAME || "balance_sheet_db",
+  database: process.env.DB_NAME || "db_can_doi",
   user: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD,
 });
